f455809408ae3da5cac9841f9ff567a8c39ebece
Bo Dong <zillionk1973@gmail.com>
2015-09-27 20:34:32 -0600

    update for controller, setting and block system.

---
+++ Assets/scripts/LevelManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	public GameObject currentCheckpoint;
+	private playerController player;
+	public GameObject[] blocks;
+	private bool flyingUp;
+
+	// Use this for initialization
+	void Start () {
+		blocks = new GameObject[25];
+		player = FindObjectOfType<playerController>();
+		blocks =  GameObject.FindGameObjectsWithTag("Block");
+		//Debug.Log (blocks[0]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.GetComponent<Rigidbody2D> ().velocity.y > 0) {
+			flyingUp = true;
+		} else if (player.GetComponent<Rigidbody2D> ().velocity.y <= 0) {
+			flyingUp = false;
+		}
+	}
+	void FixedUpdate () {
+		foreach (GameObject block in blocks) {
+			if(block.GetComponent<Collider2D>() != null) {
+				block.GetComponent<Collider2D>().isTrigger = flyingUp;
+			}
+		}
+	}
+
+	public void respawnPlayer () {
+		Debug.Log ("played respawn.");
+		player.transform.position = currentCheckpoint.transform.position;
+	}
+}
---
+++ Assets/scripts/playerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+	Rigidbody2D r_body;
+	public float moveSpeed;
+	public float jumpHeight;
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+
+	private bool grounded;
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		r_body = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+
+	void FixedUpdate () {
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space) && grounded) {
+			r_body.velocity = new Vector2(r_body.velocity.x,jumpHeight);
+		}
+		if (Input.GetKey (KeyCode.D) ) {
+			r_body.velocity = new Vector2(moveSpeed,r_body.velocity.y);
+
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			r_body.velocity = new Vector2(-moveSpeed,r_body.velocity.y);
+		}
+
+		anim.SetFloat ("Speed", Mathf.Abs (r_body.velocity.x));
+		anim.SetBool ("Grounded", grounded);
+		if (r_body.velocity.x > 0)
+			transform.localScale = new Vector3 (1f, 1f, 1f);
+		else if (r_body.velocity.x < 0)
+			transform.localScale = new Vector3 (-1f, 1f, 1f);
+
+	}
+}
